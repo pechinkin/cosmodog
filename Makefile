bdb5a3f67b33c8dcbc53475c39ca35052e62ffea
Daniil Pechinkin <pomor44@gmail.com>
2023-06-23 16:17:01 +0300

    makefile+sfml window

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ main.cpp -I//opt/homebrew/Cellar/sfml/2.5.1_2/include/ -o prog -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(500, 500), "cosmodog");
+    window.setFramerateLimit(60);
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
+
