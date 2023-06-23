d8657528fcc1d589604997efdfcfcc97b3344d70
Daniil Pechinkin <pomor44@gmail.com>
2023-06-23 17:58:40 +0300

    moving point

---
+++ Point.hpp
@@ -0,0 +1,30 @@
+#pragma once
+using vector_t = sf::Vector2f;
+
+class Point{
+    
+private:
+    vector_t m_speed;
+    vector_t m_position;
+    const float m_mass;
+public:
+    
+    Point(float x, float y, float mass, float vx, float vy): m_mass{mass} {
+        m_position.x = x;
+        m_position.y = y;
+        m_speed.x = vx;
+        m_speed.y = vy;
+    };
+    
+    void update(vector_t acceleration, float time){
+        m_position.x += m_speed.x * time;
+        m_position.y += m_speed.y * time;
+        std::cout << m_position.x << ' ' << m_position.y << ' ' << time << std::endl;
+        m_speed.x += acceleration.x * time;
+        m_speed.y += acceleration.y * time;
+    };
+    
+    vector_t getPosition(){
+        return m_position;
+    };
+};
--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Point.hpp"
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
 
 int main()
 {
+
+    sf::CircleShape spaceship; //point of spaceship
+    spaceship.setRadius(10);
+    Point dog(30, 20, 3, 20, 20);
     sf::RenderWindow window(sf::VideoMode(500, 500), "cosmodog");
     window.setFramerateLimit(60);
-    std::cout << "privet matvey\n";
+    sf::Clock clock; //to measure time of the cycle
     while (window.isOpen())
     {
+        sf::Time loop = clock.getElapsedTime();
+        clock.restart();
+        dog.update(vector_t(0,0), loop.asSeconds());
+        spaceship.setPosition(dog.getPosition());
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        
         window.clear();
+        window.draw(spaceship);
         window.display();
+
     }
 
     return 0;
