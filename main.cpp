4d5580083938e13e7644e451bad50369d30125b4
matmuher <barbusas.mail@gmail.com>
2023-06-26 02:15:08 +0300

    Add dog-planet intersection

--- main.cpp
+++ main.cpp
@@ -10,49 +10,58 @@ int main()
 {
     std::vector<Planet> planets
     {
-        {{80, 80}, 10, color_t{220, 220, 220}, 100}, // Mercury
-        {{120, 120}, 15, color_t{220, 20, 60}, 1200}, // Venus
-        {{250, 250}, 14, color_t::Green, 3000}, // Earth
-        {{330, 330}, 20, color_t::Red, 1500}  // Mars
+        {{80, 150}, 10, color_t{220, 220, 220}, 1000}, // Mercury
+        {{120, 250}, 15, color_t{220, 20, 60}, 12000}, // Venus
+        {{250, 250}, 14, color_t::Green, 20000}, // Earth
+        {{330, 450}, 20, color_t::Red, 15000}  // Mars
     };
 
     Planet satellite(vector_t{100, 100});
     
-    vector_t dog_position{50, 50};
-    vector_t velocity = computeVelocityForRotating( satellite.getPosition(),
-                                                    dog_position,
-                                                    satellite.getAcceleration(dog_position));
-    velocity = velocity * 1.0f;
-    
+    vector_t dog_position{300, 100};
+    vector_t dog_velocity{0, 0};
+
     shape_t dog_shape;
-    Point dog(dog_position, velocity, 1, dog_shape);
+    Point dog(dog_position, dog_velocity, 1, dog_shape);
     
     sf::RenderWindow window(sf::VideoMode(500, 500), "cosmodog");
     window.setFramerateLimit(60);
     sf::Clock clock; //to measure time of the cycle
     while (window.isOpen())
     {
-        sf::Time loop = clock.getElapsedTime();
+    // [events]
+
+        float time = clock.getElapsedTime().asSeconds();
         clock.restart();
-        float time = loop.asSeconds();
-        
-        vector_t acceleration{};
 
-        vector_t dog_position = dog.getPosition();
-        for (const Planet& planet : planets)
-        {
-            acceleration += planet.getAcceleration(dog_position);
-        }
-
-        dog.update(acceleration, time);
-        dog.processInput(time);
-        
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        dog.processInput(time);
+
+    // [update model]
+
+        vector_t acceleration{};
+
+        vector_t dog_position = dog.getPosition();
+        for (const Planet& planet : planets)
+        {
+            if (AreIntersect(dog_position, planet.getPosition(), planet.getRadius()))
+            {
+                std::cout << "Game Over\n";
+                exit(1);
+            }
+
+            acceleration += planet.getAcceleration(dog_position);
+        }
+
+        dog.update(acceleration, time);
+
+    // [draw]
 
         window.clear();
         window.draw(dog_shape);
